7ffd7b028b092a6c440d1ce53238f6f0b513e471
eugenii <eugenii.cher@gmail.com>
2026-04-17 20:04:52 +0300

    refrogerator

--- sprint_01/final_refrigerator.py
+++ sprint_01/final_refrigerator.py
@@ -2,7 +2,7 @@
 from decimal import Decimal
 import datetime
 
-DATE_FORMAT = '%d.%m.%Y'
+DATE_FORMAT = '%Y-%m-%d'
 
 
 def add(items, title, amount, expiration_date=None):
@@ -15,19 +15,50 @@ def add(items, title, amount, expiration_date=None):
 
 
 def add_by_note(items, note):
-    ...
+    note = note.split()
+    try:
+        datetime.datetime.strptime(note[-1], DATE_FORMAT)
+        expiration_date = note[-1]
+        amount = Decimal(note[-2])
+        title = ' '.join(note[:-2])
+    except ValueError:
+        expiration_date = None
+        amount = Decimal(note[-1])
+        title = ' '.join(note[:-1])
+    add(items, title, amount, expiration_date)
 
 
 def find(items, needle):
-    ...
+    result = []
+    for title in items:
+        if needle in title.lower():
+            result.append(title)
+    # result = [item for item in items.keys() if needle in item.lower()]
+    return result
 
 
 def get_amount(items, needle):
-    ...
+    result = Decimal('0')
+    for item in items:
+        if needle.lower() in item.lower():
+            for part in items[item]:
+                result += part['amount']
+    return result
 
 
 def get_expired(items, in_advance_days=0):
-    ...
+    result = {}
+    exp_datetime = datetime.datetime.now() + datetime.timedelta(days=in_advance_days)
+    exp_date = exp_datetime.date()
+    for item in items:
+        for part in items[item]:
+            if part['expiration_date']:
+                if part['expiration_date'] <= exp_date:
+                    if item in result:
+                        result[item] += part['amount']
+                    else:
+                        result[item] = part['amount']
+    return list(result.items())
 
 
 goods = {
@@ -43,7 +74,14 @@ goods = {
 }
 
 
-add(goods, "Cabbage", '1.1', expiration_date='12.08.1975')
-
+add(goods, "Cabbage", '1.1', expiration_date='2006-08-12')
+add_by_note(goods, 'Яйца гусиные 4 2023-07-15')
+add_by_note(goods, 'Макароны 1.5')
 print(*goods)
 print(goods['Cabbage'])
+print(find(goods, 'пель'))
+print(get_amount(goods, 'пель'))
+add_by_note(goods, 'просто пельмени 0.5 2023-07-15')
+print(find(goods, 'пель'))
+print("==================")
+print(get_expired(goods, 10))
