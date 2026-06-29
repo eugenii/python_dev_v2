26abd6bc92932bae68a94c78809778c5865bd1a5
eugenii <eugenii.cher@gmail.com>
2026-06-29 19:48:03 +0300

    a

--- Dev/jump_into_django/acme_project/acme_project/settings.py
+++ Dev/jump_into_django/acme_project/acme_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'homepage.apps.HomepageConfig',
+    'ice_cream.apps.IceCreamConfig',
     'catalog.apps.CatalogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
